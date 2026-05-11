562664c37196efe915fdfacdda10f94dc521b4fe
Flia <miracleshift@gmail.com>
2026-05-11 07:40:20 +0200

    Fix: adding a second tail gene was resulting in broken pawn state
    Adding a second tail gene would get pawn stuck in a broken state with fishtail hediffs but without either tail gene. This would prevent adding any new tails until hediff was removed.

---
+++ Source/StagzMerfolk/HediffComps/HediffComp_RemovesFishtail.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using Verse;
+
+namespace StagzMerfolk;
+
+[UsedImplicitly]
+public class HediffComp_RemovesFishtail : HediffComp
+{
+    public override void CompPostPostRemoved()
+    {
+        base.CompPostPostRemoved();
+        foreach (var gene in parent.pawn.genes.GenesListForReading.OfType<Stagz_Gene_Tail_Fish>())
+        {
+            parent.pawn.genes.RemoveGene(gene);
+        }
+    }
+}
