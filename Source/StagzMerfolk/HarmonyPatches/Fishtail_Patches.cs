2e1fae28ce08e103ab7cc579f7fef54078258a9b
Flia <miracleshift@gmail.com>
2026-07-29 17:03:59 +0200

    Fix: rare pawn generation red error
    Tricky error which I spent a good while chasing. Error happened when pawn generated with an implant on its right leg - Pawn_HealthTracker.RestorePartRecursiveInt would get negative index error because fishtail hediff already restored parts on its own. I created a separate Harmony patch to handle just pawn generation. Because I now explicitly handle it, merfolk can now generate with leg implants replacing tail; smaller implants are still overwritten by fishtail.

--- Source/StagzMerfolk/HarmonyPatches/Fishtail_Patches.cs
+++ Source/StagzMerfolk/HarmonyPatches/Fishtail_Patches.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using HarmonyLib;
 using RimWorld;
 using Verse;
 
@@ -6,7 +7,7 @@ namespace StagzMerfolk.HarmonyPatches;
 
 //Tricks Rimworld into thinking that merfolk don't have leg bodyparts suitable for wearing pants
 [HarmonyPatch(typeof(ApparelUtility), nameof(ApparelUtility.HasPartsToWear))]
-public static class ApparelUtility_FishtailPatch
+public static class FishtailPatch_ApparelUtility
 {
     public static bool Prefix(Pawn p, ThingDef apparel, ref bool __result)
     {
@@ -14,5 +15,39 @@ public static class ApparelUtility_FishtailPatch
         if (apparel.apparel.bodyPartGroups.CoversMoreThanJustLegs()) return true;
         __result = false;
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(PawnTechHediffsGenerator), nameof(PawnTechHediffsGenerator.GenerateTechHediffsFor))]
+public static class FishtailPatch_PawnTechHediffsGenerator
+{
+    public static void Postfix(Pawn pawn)
+    {
+        if (pawn?.genes?.GetFirstGeneOfType<Stagz_Gene_Tail_Fish>() is null) return;
+        
+        //If pawn generated with a leg implant, fishtail and bodyfins are removed/not added.
+        //For now only skips full leg implants, if e.g. a foot is replaced it still gets overwritten by fishtail.
+        foreach (var hediff in pawn.health.hediffSet.hediffs)
+        {
+            if (hediff.def.countsAsAddedPartOrImplant && hediff.Part.def == BodyPartDefOf.Leg)
+            {
+                var finGene = pawn.genes.GetGene(StagzDefOf.Stagz_BodyFin);
+                if (finGene is not null) pawn.genes.RemoveGene(finGene);
+                foreach (var tailGene in pawn.genes.GenesListForReading.OfType<Stagz_Gene_Tail_Fish>())
+                {
+                    pawn.genes.RemoveGene(tailGene);
+                }
+
+                return;
+            }
+        }
+        
+        //otherwise finish adding tail
+        pawn.RemoveLegOnlyApparel();
+        foreach (var leg in pawn.RaceProps.body.GetPartsWithDef(BodyPartDefOf.Leg))
+        {
+            pawn.health.RestorePart(leg, null, false);
+            pawn.health.AddHediff(StagzDefOf.Stagz_Tail, leg);
+        }
     }
 }
